254ea7b057a7ef88d8caa7c5f1646fea64d90852
João Pedro Sousa Gaspar <81166690+joaogaspar00@users.noreply.github.com>
2021-03-22 13:12:36 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,15 +6,15 @@
 * Autor: João Pedro Sousa Gaspar											  
 *																			  
 * Âmbito: A resolução do trabalho foi feita com o objetivo de ajudar os meus  
-* 		mentorandos na intrepretação e desenvolvimento do seu programa, sem   
-* 		mostrar a codificação.												  
+* 	mentorandos na intrepretação e desenvolvimento do seu programa, sem   
+* 	mostrar a codificação.												  
 *																			  
 * Descrição: Desenvolver um program em C, norma ansi, com a finalidade de     
-* 		calcular distâncias de rotas. As rotas são definidas através de uma   
-* 		sequência de nomes de localidades. Todas as possíveis localidades 	  
-* 		estão guardadas num ficheiro de texto (base de dados) cujo nome é 	  
-* 		localidades.txt. As rotas são especificadas num ficheiro de texto     
-* 		(rotas.txt) que o programa deverá processar. 						  
+*   calcular distâncias de rotas. As rotas são definidas através de uma   
+* 	sequência de nomes de localidades. Todas as possíveis localidades 	  
+* 	estão guardadas num ficheiro de texto (base de dados) cujo nome é 	  
+* 	localidades.txt. As rotas são especificadas num ficheiro de texto     
+* 	(rotas.txt) que o programa deverá processar. 						  
 *																			  
 *******************************************************************************
 * VERSÕES:																	  
